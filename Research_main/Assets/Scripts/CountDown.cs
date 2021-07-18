0153abd946df0d4ac3768f0e51d8b9e876191b88
CHELara7 <a.428clover@gmail.com>
2021-07-18 17:15:00 +0900

    cannot audio

--- Research_main/Assets/Scripts/CountDown.cs
+++ Research_main/Assets/Scripts/CountDown.cs
@@ -7,6 +7,10 @@ public class CountDown : MonoBehaviour
 {
 
     [SerializeField] private Text countdownText;
+    [SerializeField] private GameObject Bgm;
+    [SerializeField] private AudioSource audio;
+    [SerializeField] private AudioClip sound1;
+    [SerializeField] private AudioClip sound2;
 
     public bool endCount = false;
     // Start is called before the first frame update
@@ -31,13 +35,17 @@ public class CountDown : MonoBehaviour
         for (int i = 2; i >= 0; i--)
         {
             countdownText.text = (i + 1).ToString();  //　文字の表示を切り替える
+            audio.PlayOneShot(sound1);                //　音を鳴らす
+            Debug.Log("oto");
             yield return new WaitForSeconds(1.5f);  //　1秒待つ
         }
         countdownText.color = new Color(1.0f, 0f, 0f, 1.0f);  //　文字を赤色へ
         countdownText.text = "START!";  //　スタート
+        audio.PlayOneShot(sound2);                //　音を鳴らす
         yield return new WaitForSeconds(2);  //　1秒待つ
         countdownText.enabled = false;  //　テキスト非表示
         //　ゲーム開始
+        Bgm.SetActive(true);
         endCount = true;
     }
 }
