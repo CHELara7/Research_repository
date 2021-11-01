d6b53f3bb44af292a67e263d4d2477cc388500f7
CHELara7 <a.428clover@gmail.com>
2021-11-01 15:58:24 +0900

    ad scooter board

--- Research_main/Assets/Scripts/CountDown.cs
+++ Research_main/Assets/Scripts/CountDown.cs
@@ -49,7 +49,7 @@ public class CountDown : MonoBehaviour
         countdownText.enabled = false;
 
         //　ゲーム開始
-        Bgm.SetActive(true);
+        //Bgm.SetActive(true);
         endCount = true;
     }
 }
